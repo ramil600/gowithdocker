9e6bedd91c1d433eb63979a1b4b6ad53f60fd9ec
Ramil Mirhasanov <ramil600@yahoo.com>
2021-09-05 13:19:09 +0300

    Added Ping Handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ramil600/gowithdocker/serviceone
 
 go 1.16
+
+require github.com/labstack/echo/v4 v4.5.0 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"time"
+
+	"github.com/labstack/echo/v4"
 	"github.com/ramil600/gowithdocker/serviceone/eurekautils"
+	"github.com/ramil600/gowithdocker/serviceone/handlers"
 )
 
 func main() {	
 	ins := eurekautils.GetNewInstance()
 	ins.RegisterService()
 	time.Sleep(10 * time.Second)
-	ins.SendHeartBeat()
-	time.Sleep(5 * time.Second)
+	
+	e := echo.New()
+	e.GET("/ping", handlers.PingHandler)
+	e.Logger.Fatal(e.Start(":8000"))
+	
+	for i:=0; i < 2; i++ {
+		ins.SendHeartBeat()
+		time.Sleep(5 * time.Second)
+	}
+
 	ins.ShutDown()
 	
 }
